3c72bfc8c0ae0118fc7e11d3ddceb8a7fd90c633
Starover1983 <a_lexei@mail.ru>
2023-08-26 12:27:37 +0300

    third upgrade homework 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 const listRef = document.querySelectorAll(".item");
 console.log("Number of categories:", listRef.length);
 
-listRef.forEach(el=>console.log(`Category:${el.firstElementChild.textContent} Elements: ${el.lastElementChild.childElementCount
+listRef.forEach(el=>console.log(`\nCategory:${el.firstElementChild.textContent} \nElements: ${el.lastElementChild.childElementCount
 }`))
